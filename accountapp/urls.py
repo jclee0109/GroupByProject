f564e140a8214ac9a400b5733c431dadb1955676
jclee0109 <jclee0109@gist.ac.kr>
2021-09-23 21:44:39 +0900

    first commit

---
+++ accountapp/urls.py
@@ -0,0 +1,9 @@
+from accountapp.views import hello_world
+from django.urls import path
+from django.urls.conf import include
+
+app_name = "accountapp"
+
+urlpatterns = [
+    path('hello_world/', hello_world, name='hello_world')
+]
---
+++ accountapp/views.py
@@ -0,0 +1,10 @@
+
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+
+def hello_world(request):
+    return HttpResponse("Hello world!")
+
+
