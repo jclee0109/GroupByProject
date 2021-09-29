c51256c74db2a6d0d9936be235e3c526e4d11390
jclee0109 <jclee0109@gist.ac.kr>
2021-09-29 14:32:45 +0900

    moved to Pycharm, FBV ->> CBV

--- accountapp/urls.py
+++ accountapp/urls.py
@@ -1,9 +1,10 @@
-from accountapp.views import hello_world
+from accountapp.views import hello_world, AccountCreateView
 from django.urls import path
 from django.urls.conf import include
 
 app_name = "accountapp"
 
 urlpatterns = [
-    path('hello_world/', hello_world, name='hello_world')
+    path('hello_world/', hello_world, name='hello_world'),
+    path('signup/', AccountCreateView.as_view(), name='signup'),
 ]
--- accountapp/views.py
+++ accountapp/views.py
@@ -1,23 +1,30 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django.views.generic import CreateView
 
 from accountapp.models import HelloWorld
-from django.http.response import HttpResponse
+from django.urls import reverse, reverse_lazy
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
+
 
 # Create your views here.
 
 def hello_world(request):
-
-    if request.method =="POST":
+    if request.method == "POST":
         temp = request.POST.get('hello_world_input')
 
         new_hello_world = HelloWorld()
         new_hello_world.text = temp
         new_hello_world.save()
 
-        return render(request, 'accountapp/hello_world.html', context={'hello_world_output':new_hello_world})
+        return HttpResponseRedirect(reverse('accountapp:hello_world'))
     else:
-        return render(request, 'accountapp/hello_world.html', context={'text':'GET METHOD!!'})
+        hello_world_list = HelloWorld.objects.all()
+        return render(request, 'accountapp/hello_world.html', context={'hello_world_output': hello_world_list})
 
-    
-
-
+class AccountCreateView(CreateView):
+    model = User
+    form_class = UserCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'accountapp/signup.html'
