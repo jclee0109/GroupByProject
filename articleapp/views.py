314537038ff6c4c4e33096f01e7a0eeae98bfadc
jclee0109 <jclee0109@gist.ac.kr>
2021-10-10 20:44:07 +0900

    articleapp crud complete

--- articleapp/views.py
+++ articleapp/views.py
@@ -4,8 +4,9 @@ from django.shortcuts import render
 # Create your views here.
 from django.urls import reverse
 from django.utils.decorators import method_decorator
-from django.views.generic import CreateView, DetailView
+from django.views.generic import CreateView, DetailView, UpdateView, DeleteView
 
+from articleapp.decorator import article_ownership_required
 from articleapp.forms import ArticleCreationForm
 from articleapp.models import Article
 
@@ -31,4 +32,27 @@ class ArticleDetailView(DetailView):
     model = Article
     context_object_name = 'target_article'
     template_name = 'articleapp/detail.html'
+
+
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
+class ArticleUpdateView(UpdateView):
+    model = Article
+    form_class = ArticleCreationForm
+    context_object_name = 'target_article'
+    template_name = 'articleapp/update.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:detail', kwargs={'pk': self.object.pk})
+
+
+@method_decorator(article_ownership_required, 'get')
+@method_decorator(article_ownership_required, 'post')
+class ArticleDeleteView(DeleteView):
+    model = Article
+    context_object_name = 'target_article'
+    template_name = 'articleapp/delete.html'
+
+    def get_success_url(self):
+        return reverse('articleapp:list')
 
