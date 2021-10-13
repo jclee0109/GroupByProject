85d58b5ac9d11f90cecc5d39a61b4d82e61e1d37
jclee0109 <jclee0109@gist.ac.kr>
2021-10-13 21:26:34 +0900

    create projectapp - need to modify

--- accountapp/views.py
+++ accountapp/views.py
@@ -37,7 +37,7 @@ class AccountCreateView(CreateView):
     model = User
     form_class = UserCreationForm
     success_url = reverse_lazy('accountapp:hello_world')
-    template_name = 'accountapp/signup.html'
+    template_name = 'accountapp/create.html'
 
 
 class AccountDetailView(DetailView):
--- groupby/settings.py
+++ groupby/settings.py
@@ -53,6 +53,7 @@ INSTALLED_APPS = [
     'profileapp',
     'articleapp',
     'commentapp',
+    'projectapp',
     'rest_framework',
     'django.contrib.sites',
     'allauth',
@@ -147,7 +148,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
